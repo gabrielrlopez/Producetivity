bfdfe05ed46ff1ac95755798fe3452213fff5f88
gabrielrlopez <gabrielrosaleslopez123@gmail.com>
2021-04-28 19:57:45 -0600

    Scraped all King Soopers produce pages and wrote all scraped data into a csv file

--- scraper.js
+++ scraper.js
@@ -1,35 +1,39 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
+const fs = require("fs");
+const writeStream = fs.createWriteStream("produce.csv");
 
-axios
-  .get(
-    "https://www.kingsoopers.com/search?query=produce&searchType=previous_searches&fulfillment=all"
-  )
-  .then((res) => {
-    const $ = cheerio.load(res.data);
+// Scrape all 12 pages of King Soopers produce items
+for (let page = 1; page <= 12; page++) {
+  axios
+    .get(
+      `https://www.kingsoopers.com/search?fulfillment=all&page=${page}&query=produce&searchType=previous_searches`
+    )
+    .then((res) => {
+      const $ = cheerio.load(res.data);
 
-    let produce = [];
+      $(".kds-Card").each((index, element) => {
+        const product = $(element)
+          .children(".flex-grow")
+          .children(".kds-Link")
+          .text();
 
-    $(".kds-Card").each((index, element) => {
-      const product = $(element)
-        .children(".flex-grow")
-        .children(".kds-Link")
-        .text();
+        const weight = $(element)
+          .children(".flex-grow")
+          .children(".kds-Text--xs")
+          .text();
 
-      const weight = $(element)
-        .children(".flex-grow")
-        .children(".kds-Text--xs")
-        .text();
+        const picture = $(element)
+          .children(".mb-4")
+          .children("a")
+          .children()
+          .children()
+          .attr("src");
 
-      const picture = $(element)
-        .children(".mb-4")
-        .children("a")
-        .children()
-        .children()
-        .attr("src");
+        //Write Row to Csv
+        writeStream.write(`${product}, ${weight}, ${picture} \n`);
+      });
 
-      produce[index] = { product, weight, picture };
+      console.log("Scraping done.");
     });
-
-    console.log(produce);
-  });
+}
