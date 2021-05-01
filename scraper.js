d46e5af53709bde1160632eb31734e83b0c431b7
gabrielrlopez <gabrielrosaleslopez123@gmail.com>
2021-04-30 19:03:23 -0600

    Created a json file with scraped data and started designing mysql table

--- scraper.js
+++ scraper.js
@@ -4,36 +4,56 @@ const fs = require("fs");
 const writeStream = fs.createWriteStream("produce.csv");
 
 // Scrape all 12 pages of King Soopers produce items
-for (let page = 1; page <= 12; page++) {
-  axios
-    .get(
-      `https://www.kingsoopers.com/search?fulfillment=all&page=${page}&query=produce&searchType=previous_searches`
-    )
-    .then((res) => {
-      const $ = cheerio.load(res.data);
+let allJSONdata = [];
+let tempData = {};
+const scrapeData = () => {
+  for (let page = 1; page <= 12; page++) {
+    console.log(`Scraping page: ${page}`);
+    axios
+      .get(
+        `https://www.kingsoopers.com/search?fulfillment=all&page=${page}&query=produce&searchType=previous_searches`
+      )
+      .then((res) => {
+        const $ = cheerio.load(res.data);
 
-      $(".kds-Card").each((index, element) => {
-        const product = $(element)
-          .children(".flex-grow")
-          .children(".kds-Link")
-          .text();
+        $(".kds-Card").each((index, element) => {
+          const product = $(element)
+            .children(".flex-grow")
+            .children(".kds-Link")
+            .text();
 
-        const weight = $(element)
-          .children(".flex-grow")
-          .children(".kds-Text--xs")
-          .text();
+          const weight = $(element)
+            .children(".flex-grow")
+            .children(".kds-Text--xs")
+            .text();
 
-        const picture = $(element)
-          .children(".mb-4")
-          .children("a")
-          .children()
-          .children()
-          .attr("src");
+          const picture = $(element)
+            .children(".mb-4")
+            .children("a")
+            .children()
+            .children()
+            .attr("src");
 
-        //Write Row to Csv
-        writeStream.write(`${product}, ${weight}, ${picture} \n`);
+          //Write Row to Csv
+          // writeStream.write(`${product}, ${weight}, ${picture} \n`);
+
+          tempData = {
+            id,
+            product,
+            weight,
+            picture,
+          };
+
+          allJSONdata.push(tempData);
+        });
+
+        saveData(allJSONdata);
       });
+  }
+};
 
-      console.log("Scraping done.");
-    });
-}
+const saveData = (data) => {
+  fs.writeFile("produce.json", JSON.stringify(data), (err) => {
+    if (err) console.log(err);
+  });
+};
